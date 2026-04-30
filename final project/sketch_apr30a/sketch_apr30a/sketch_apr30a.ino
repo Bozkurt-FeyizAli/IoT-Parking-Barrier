52912d8d4848249a6cc32f8f6a72e01476a0b931
Bozkurt-FeyizAli <Feyizalibozkurt@gmail.com>
2026-04-30 21:44:54 +0300

    add hc-sr04

--- final project/sketch_apr30a/sketch_apr30a/sketch_apr30a.ino
+++ final project/sketch_apr30a/sketch_apr30a/sketch_apr30a.ino
@@ -2,6 +2,16 @@
 #include <MFRC522.h>
 #include <ESP32Servo.h> // ESP32 için özel servo kütüphanesi
 
+const int trigPin = 13;
+const int echoPin = 12;
+
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+int distanceCm;
+int distanceInch;
+
 // ESP32 30-Pin Standart Bağlantıları
 #define SS_PIN    5   // RC522 SDA (SS)
 #define RST_PIN   4  // RC522 Reset
@@ -20,18 +30,50 @@ void setup() {
   SPI.begin();          // Standart SPI başlatma (SCK:18, MISO:19, MOSI:23)
   rfid.PCD_Init();
   
+  pinMode(sensor1Pin, INPUT);
+  pinMode(sensor2Pin, INPUT);
 
   motor.setPeriodHertz(50);           // Standart 50Hz servo
   motor.attach(SERVO_PIN, 500, 2400); // Pin ve darbe genişlikleri
   motor.write(0); 
 
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 
+  
   delay(500);
 
   
 }
 
 void loop() {
+
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate the distance
+  distanceCm = duration * SOUND_SPEED/2;
+  
+  // Convert to inches
+  distanceInch = distanceCm * CM_TO_INCH;
+  
+  int sensor1Value = digitalRead(sensor1Pin);
+  int sensor2Value = digitalRead(sensor2Pin);
+  // Prints the distance in the Serial Monitor
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  Serial.print("Distance (inch): ");
+  Serial.println(distanceInch);
+
+
 
 
   if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
