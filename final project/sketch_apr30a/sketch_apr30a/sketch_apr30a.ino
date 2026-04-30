d0d37d944292a866abeef828cdfc5355ac1dd40a
Bozkurt-FeyizAli <Feyizalibozkurt@gmail.com>
2026-04-30 21:40:58 +0300

    try to start with just rfid and motor

---
+++ final project/sketch_apr30a/sketch_apr30a/sketch_apr30a.ino
@@ -0,0 +1,67 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <ESP32Servo.h> // ESP32 için özel servo kütüphanesi
+
+// ESP32 30-Pin Standart Bağlantıları
+#define SS_PIN    5   // RC522 SDA (SS)
+#define RST_PIN   4  // RC522 Reset
+#define SERVO_PIN 26  // Servo Sinyal Pini
+
+#define sensor1Pin 27
+#define sensor2Pin 14
+
+Servo motor;
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+byte ID[4] = {40, 130, 1, 14}; // Yetkili Kart ID
+
+void setup() {
+  Serial.begin(115200);
+  SPI.begin();          // Standart SPI başlatma (SCK:18, MISO:19, MOSI:23)
+  rfid.PCD_Init();
+  
+
+  motor.setPeriodHertz(50);           // Standart 50Hz servo
+  motor.attach(SERVO_PIN, 500, 2400); // Pin ve darbe genişlikleri
+  motor.write(0); 
+
+
+  delay(500);
+
+  
+}
+
+void loop() {
+
+
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
+    return;
+  
+  if (rfid.uid.uidByte[0] == ID[0] && 
+      rfid.uid.uidByte[1] == ID[1] &&
+      rfid.uid.uidByte[2] == ID[2] &&
+      rfid.uid.uidByte[3] == ID[3]) {
+    
+    Serial.println("Kapi acildi");
+    ekranaYazdir();
+    motor.write(180);
+    delay(3000);
+    motor.write(0);
+    delay(1000);
+  } else {
+    Serial.println("Yetkisiz Kart");
+    ekranaYazdir();
+  }
+  rfid.PICC_HaltA();
+
+
+}
+
+void ekranaYazdir() {
+  Serial.print("ID Numarasi: ");
+  for (int sayac = 0; sayac < 4; sayac++) {
+    Serial.print(rfid.uid.uidByte[sayac]);
+    Serial.print(" ");
+  }
+  Serial.println("");
+}
